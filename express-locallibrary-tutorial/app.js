c7ba347fe306a85c28497b58eeec2c0e51aadd93
MicheledG <chiefzephyr@gmail.com>
2017-12-30 16:04:46 +0100

    added routes and dummy controllers

--- express-locallibrary-tutorial/app.js
+++ express-locallibrary-tutorial/app.js
@@ -26,8 +26,10 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+// import routes
 var index = require('./routes/index');
 var users = require('./routes/users');
+var catalog = require('./routes/catalog')
 
 var app = express();
 
@@ -45,6 +47,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+app.use('/catalog', catalog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
